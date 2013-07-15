797d0700d8f839eb98442af5be69953214db5b1d
ugo_dock <ugo_dock@ugo-dock>
2013-07-15 23:13:56 +0200

    Updated netbeans.xml file

---
+++ RegistrationMgt/src/main/java/com/springinaction/springidol/NewInterface.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.springinaction.springidol;
+
+/**
+ *
+ * @author ugo_dock
+ */
+public interface NewInterface {
+    
+}
